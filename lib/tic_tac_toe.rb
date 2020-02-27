f938dffb53b13f6545567679cd2fa371272edd30
Haley Kelling <haley@kellings.com>
2020-02-27 17:07:37 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,4 +32,9 @@ class TicTacToe
   def position_taken?(index)
     !(@board[index] == " " || @board[index] == "" || @board[index] == nil)
   end
+
+  def valid_move?(index)
+    index.between?(0, 8) && position_taken?(index) == false
+  end
+
 end

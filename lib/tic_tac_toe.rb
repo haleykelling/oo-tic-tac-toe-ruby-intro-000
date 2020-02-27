7e4be78bcb9927fe07182bfbff872a71cb8dc65c
Haley Kelling <haley@kellings.com>
2020-02-27 16:55:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,7 +25,7 @@ class TicTacToe
     board.count{|token| token == "X" || token == "O"}
   end
 
-  def move(index, value = "X")
+  def move(@index, value = "X")
     @board[@index] = value
   end
 end

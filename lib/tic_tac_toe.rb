5d9982d657d60aa715ac65b10f397f23d2e6c407
Haley Kelling <haley@kellings.com>
2020-02-27 17:12:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,4 +51,67 @@ class TicTacToe
     display_board
   end
 
+  def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    else
+      false
+    end
+  end
+  if false
+    return false
+  end
+end
+
+def full?(board)
+  board.each do |index|
+    if index == "X"
+      true
+    elsif index == "O"
+      true
+    else
+      return false
+    end
+  end
+  if true
+    return true
+  end
+end
+
+def draw?(board)
+  if full?(board) == true && won?(board) == nil
+    return true
+  else
+    return false
+  end
+end
+
+def over?(board)
+  if won?(board) != nil || draw?(board) == true || full?(board) == true
+    return true
+  else
+    return false
+  end
+end
+
+def winner(board)
+  if won?(board) != nil
+    return board[won?(board)[0]]
+  else
+    return nil
+  end
+end
+
+
 end
